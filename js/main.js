60bd30d54b6dbd43a1940c78ea1aa89d7634759a
marocena26 <mariaelena.arocena@gmail.com>
2022-11-03 11:03:59 +0100

    start with the new partner, sprint 2, created structure and add coment

---
+++ js/main.js
@@ -0,0 +1,140 @@
+'use strict';
+//-----------------------VARIABLES----------------------------//
+/* Elementos que usamos en el HTML */
+const newFormElement = document.querySelector('.js-new-form');
+const listElement = document.querySelector('.js-list');
+const searchButton = document.querySelector('.js-button-search');
+const buttonAdd = document.querySelector('.js-btn-add');
+const buttonCancelForm = document.querySelector('.js-btn-cancel');
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const linkNewFormElememt = document.querySelector('.js-button-new-form');
+const labelMesageError = document.querySelector('.js-label-error');
+const input_search_desc = document.querySelector('.js_in_search_desc');
+
+//-----------------------OBJETOS----------------------------//
+//Objetos con cada gatito
+const kittenData_1 = {
+    image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+    name: "Anastacio",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
+};
+const kittenData_2 = {
+    image: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+    name: "Fiona",
+    desc: "Juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
+};
+const kittenData_3 = {
+    image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+    name: "Cielo",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
+};
+
+const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
+
+//-----------------------FUNCIONES----------------------------//
+
+//Para crear la función general de los gatos cogemos el ul (los li son los gatitos) y creamos el esqueleto con todos los elementos que lo contienen. Utilizamos las variables de forma neutra (sin contenido $(contenido)) para después cambiarlas con los datos que queramos. 
+
+function renderKitten(kittenData) {
+    const kitten = `<li class="card">
+    <article>
+      <img
+        class="card_img"
+        src=${kittenData.image}
+        alt="gatito"
+      />
+      <h3 class="card_title">${kittenData.name}</h3>
+      <h3 class="card_race">${kittenData.race}</h3>
+      <p class="card_description">
+      ${kittenData.desc}
+      </p>
+    </article>
+    </li>`;
+    return kitten;
+}
+
+//Bucle for...of: Creamos una nueva función renderKittenList(kittenDataList) que recibirá como parámetro el listado de gatitos que creamos en kittenDataList, y se utilizará la función renderKitten(kittenItem).
+
+
+function renderKittenList(kittenDataList) {
+    listElement.innerHTML = "";
+    for (const kittenItem of kittenDataList) {
+        listElement.innerHTML += renderKitten(kittenItem);
+    }
+}
+
+//Esto quita el buscador
+function showNewCatForm() {
+    newFormElement.classList.remove('collapsed');
+}
+
+//Esto añade el buscador
+function hideNewCatForm() {
+    newFormElement.classList.add('collapsed');
+}
+
+//Esto es la función manejadora
+function handleClickNewCatForm(event) {
+    event.preventDefault();
+    if (newFormElement.classList.contains('collapsed')) {
+        showNewCatForm(); //aqui llaman a la función show
+    } else {
+        hideNewCatForm(); //aqui llaman a la función hiden
+    }
+}
+//Adicionar nuevo gatito: hemos creado una función para añadir gatitos, lo hacemos cuando clickamos en añadir. Para eso hemos tenido que crear un addEventListener.
+
+function addNewKitten(event) {
+    event.preventDefault();
+    const valueDesc = inputDesc.value;
+    const valuePhoto = inputPhoto.value;
+    const valueName = inputName.value;
+    if (valueDesc === "" && valuePhoto === "" && valueName === "") {
+        labelMesageError.innerHTML = "Debe rellenar todos los valores";
+    } else {
+        if (valueDesc !== "" && valuePhoto !== "" && valueName !== "") {
+            labelMesageError.innerHTML = "";
+        }
+    }
+}
+//Cancelar la búsqueda de un gatito: hemos creado una función para cancelar, lo hacemos cuando clickamos en cancelar. Para eso hemos tenido que crear un addEventListener.
+
+function cancelNewKitten(event) {
+    event.preventDefault();
+    newFormElement.classList.add("collapsed");
+    inputDesc.value = "";
+    inputPhoto.value = "";
+    inputName.value = "";
+}
+
+//Filtrar por descripción
+function filterKitten(event) {
+    event.preventDefault();
+    const descrSearchText = input_search_desc.value;
+    listElement.innerHTML = "";
+    for (const kittenItem of kittenDataList) {
+        if (kittenItem.desc.includes(descrSearchText)) {
+            listElement.innerHTML += renderKitten(kittenItem);
+        }
+    }
+}
+
+//Mostrar el litado de gatitos en ell HTML
+renderKittenList(kittenDataList);
+
+//Eventos
+linkNewFormElememt.addEventListener("click", handleClickNewCatForm);
+searchButton.addEventListener("click", filterKitten);
+buttonAdd.addEventListener("click", addNewKitten);
+buttonCancelForm.addEventListener("click", cancelNewKitten);
+
+
+
+
+
+
