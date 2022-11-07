199d5385b67f1b5a5b542b05a996c357c025137c
marocena26 <mariaelena.arocena@gmail.com>
2022-11-07 11:05:41 +0100

    do fetch server and filter kitten description and race

--- js/main.js
+++ js/main.js
@@ -13,29 +13,31 @@ const inputRace = document.querySelector('.js-input-race');
 const linkNewFormElememt = document.querySelector('.js-button-new-form');
 const labelMesageError = document.querySelector('.js-label-error');
 const input_search_desc = document.querySelector('.js_in_search_desc');
+const input_search_race = document.querySelector('.js_in_search_race');
 
 //-----------------------OBJETOS----------------------------//
 //Objetos con cada gatito
-const kittenData_1 = {
-    image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
-    name: "Anastacio",
-    desc: "Risueño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-    race: "British Shorthair",
-};
-const kittenData_2 = {
-    image: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
-    name: "Fiona",
-    desc: "Juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-    race: "British Shorthair",
-};
-const kittenData_3 = {
-    image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
-    name: "Cielo",
-    desc: "Risueño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-    race: "British Shorthair",
-};
+// const kittenData_1 = {
+//     image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+//     name: "Anastacio",
+//     desc: "Risueño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+//     race: "British Shorthair",
+// };
+// const kittenData_2 = {
+//     image: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+//     name: "Fiona",
+//     desc: "Juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+//     race: "British Shorthair",
+// };
+// const kittenData_3 = {
+//     image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+//     name: "Cielo",
+//     desc: "Risueño, guapo, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+//     race: "Gatito",
+// };
 
-const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
+let kittenDataList = [];
+//const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
 
 //-----------------------FUNCIONES----------------------------//
 
@@ -124,22 +126,38 @@ function cancelNewKitten(event) {
 }
 
 
-
 //Filtrar por descripción
 function filterKitten(event) {
     event.preventDefault();
     const descrSearchText = input_search_desc.value;
+    const raceSearchText = input_search_race.value; 
     listElement.innerHTML = "";
-    /*for (const kittenItem of kittenDataList) {
+
+    const filterDescription = kittenDataList.filter((kittenItem) => {
         if (kittenItem.desc.includes(descrSearchText)) {
             listElement.innerHTML += renderKitten(kittenItem);
         }
-    }*/
-
-    const kittenDataListFilter = kittenDataList.filter((kitten) => kitten.desc.includes === descrSearchText);
-    listElement.innerHTML += renderKittenList(kittenDataListFilter);
+    })
+    .filter((kittenItem) => {
+        if (kittenItem.race.includes(raceSearchText)) {
+            listElement.innerHTML += renderKitten(kittenItem);
+        }
+    });
 }
 
+//PETICIONES AL SERVIDOR 
+
+const GITHUB_USER = 'marocena26';
+const SERVER_URL = `https://dev.adalab.es/api/kittens/${GITHUB_USER}`;
+
+fetch(SERVER_URL, {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'},
+  })
+    .then((response) => response.json())
+    .then((data) => {kittenDataList = data.results
+    renderKittenList(kittenDataList)}
+)
 //Mostrar el litado de gatitos en el HTML
 renderKittenList(kittenDataList);
 
