80866be0b19fa7cdc691fce865e71ef5b8a77a28
marocena26 <mariaelena.arocena@gmail.com>
2022-11-10 11:02:25 +0100

    finish exercises DOM

--- js/main.js
+++ js/main.js
@@ -45,21 +45,57 @@ let kittenDataList = [];
 //Para crear la función general de los gatos cogemos el ul (los li son los gatitos) y creamos el esqueleto con todos los elementos que lo contienen. Utilizamos las variables de forma neutra (sin contenido $(contenido)) para después cambiarlas con los datos que queramos. 
 
 function renderKitten(kittenData) {
-    const kitten = `<li class="card">
-    <article>
-      <img
-        class="card_img"
-        src=${kittenData.image}
-        alt="gatito"
-      />
-      <h3 class="card_title">${kittenData.name}</h3>
-      <h3 class="card_race">${kittenData.race}</h3>
-      <p class="card_description">
-      ${kittenData.desc}
-      </p>
-    </article>
-    </li>`;
-    return kitten;
+    const liElement = document.createElement('li');//creación del <li>
+    liElement.classList.add('card');
+    
+    const articleElement = document.createElement('article');//creamos el <article>
+    
+    const imgElement = document.createElement('img'); //creamos imagen
+    imgElement.classList.add('card_img');
+    imgElement.setAttribute('src', kittenData.image);//metemos imagen
+    imgElement.setAttribute('alt', 'gatito');
+    
+    const h3NameElement = document.createElement('h3');//creamos <h3> del nombre
+    h3NameElement.classList.add('card_title');
+    const titleName = document.createTextNode(kittenData.name);//añadimos nombre
+    h3NameElement.appendChild(titleName);//añadimos el nombre en el <h3>
+    
+    const h3RaceElement = document.createElement('h3');//creamos <h3> de la raza
+    h3RaceElement.classList.add('card_race');
+    const titleRace = document.createTextNode(kittenData.race);//añadimos raza
+    h3RaceElement.appendChild(titleRace); //añadimos la raza en el <h3>
+    
+    const descElement = document.createElement('p');//cremamos el <p> de desc
+    descElement.classList.add('card_description');
+    const descText = document.createTextNode(kittenData.desc);//añadimos desc
+    descElement.appendChild(descText);//añadimos desc al <p>
+    
+    liElement.appendChild(articleElement);//<article> dentro de nuestro <li>
+    articleElement.appendChild(imgElement); //elementos dentro de nuestro <article>
+    articleElement.appendChild(h3NameElement);
+    articleElement.appendChild(h3RaceElement);
+    articleElement.appendChild( descElement);
+    console.log(liElement);
+
+    return liElement;
+
+       
+
+    // const kitten = `<li class="card">
+    // <article>
+    //   <img
+    //     class="card_img"
+    //     src=${kittenData.image}
+    //     alt="gatito"
+    //   />
+    //   <h3 class="card_title">${kittenData.name}</h3>
+    //   <h3 class="card_race">${kittenData.race}</h3>
+    //   <p class="card_description">
+    //   ${kittenData.desc}
+    //   </p>
+    // </article>
+    // </li>`;
+    // return kitten;
 }
 
 //Bucle for...of: Creamos una nueva función renderKittenList(kittenDataList) que recibirá como parámetro el listado de gatitos que creamos en kittenDataList, y se utilizará la función renderKitten(kittenItem).
@@ -68,7 +104,7 @@ function renderKitten(kittenData) {
 function renderKittenList(kittenDataList) {
     listElement.innerHTML = "";
     for (const kittenItem of kittenDataList) {
-        listElement.innerHTML += renderKitten(kittenItem);
+        listElement.appendChild(renderKitten(kittenItem));
     }
 }
 
@@ -165,7 +201,7 @@ function filterKitten(event) {
 
 //PETICIONES AL SERVIDOR 
 
-const GITHUB_USER = 'marocena26';
+const GITHUB_USER = 'ElenaRentero';
 const SERVER_URL = `https://dev.adalab.es/api/kittens/${GITHUB_USER}`;
 
 /*fetch(SERVER_URL, {
